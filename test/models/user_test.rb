3025a0ce0875160cc78ca2b5d32bbb7912959f53
Jeffrey Hardy <jeff@37signals.com>
2024-09-19 13:57:23 -0400

    Test user creation and deactivation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "create" do
+    user = User.create! \
+      account: accounts("37s"),
+      name: "Victor Cooper",
+      email_address: "victor@37signals.com",
+      password: "secret123456"
+
+    assert_equal accounts("37s"), user.account
+    assert_equal user, User.authenticate_by(email_address: "victor@37signals.com", password: "secret123456")
+  end
+
+  test "deactivate" do
+    assert_changes -> { users(:jz).active? }, from: true, to: false do
+      users(:jz).deactivate
+    end
+  end
 end
